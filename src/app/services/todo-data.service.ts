47f6acf8c9d146c42ca92c39598025953032e789
sudiptoroy1985 <kolorblind.85@gmail.com>
2017-04-02 13:39:31 +0200

    modified toggle behaviour todo

--- src/app/services/todo-data.service.ts
+++ src/app/services/todo-data.service.ts
@@ -19,32 +19,32 @@ export class TodoDataService {
 
    deleteTodoById(id:number):TodoDataService{
      this.todos = this.todos.filter(t => t.id !== id);
-     return this;     
+     return this;
    }
 
 
    getTodoById(id:number) : Todo{
      return this.todos.filter(t=>t.id==id).pop();
    }
-    
-  //  updateTodoById(id:number): Todo{
-  //    let todo = this.getTodoById(id);
-  //    if(!todo)
-  //    {
-  //      return null;
-  //    }
-  //    Object.assign(todo,values);
-  //    return todo;     
-  //  }
+
+    updateTodoById(id:number,values: Object = {}): Todo{
+     let todo = this.getTodoById(id);
+     if(!todo)
+     {
+       return null;
+     }
+     Object.assign(todo,values);
+     return todo;
+   }
 
    getAllTodos(): Todo[]{
      return this.todos;
    }
 
     toggleTodoComplete(todo:Todo){
-    //  let updatedTodo = this.updateTodoById(todo.id, {
-    //     complete: !todo.complete
-    //  });
+      let updatedTodo = this.updateTodoById(todo.id, {
+        complete: !todo.complete
+      });
 
     }
 
